232cfdc1370552d8af0a3fe447401ff0bf0d0c24
Ravano Akbar Widodo <118270814+ravano-2464@users.noreply.github.com>
2024-01-29 07:24:00 +0700

    Bootcamp Day 14 Personal Web

--- index.js
+++ index.js
@@ -1,201 +1,281 @@
-const express = require('express');
-const { Sequelize, QueryTypes } = require('sequelize');
-const session = require('express-session');
-const flash = require('express-flash');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const flash = require("express-flash");
 const app = express();
 const port = 7000;
 
-const { development } = require('./src/assets/config/config.json');
+// const dbPool = require("./src/connection/database");
+
+// sequalize
+const { development } = require("./src/config/config.json");
+const { Sequelize, QueryTypes } = require("sequelize");
 const SequelizePool = new Sequelize(development);
 
-app.set('view engine', 'hbs');
-app.set('views', 'src/views');
+// test db
+// dbPool.connect((err) => {
+//   if (err) {
+//     console.log(err.message);
+//   } else {
+//     console.log("Database Connected");
+//   }
+// });
 
-app.use('/assets', express.static('src/assets'));
-app.use(express.urlencoded({ extended: false }));
-
-app.use(session({
+// use hbs for view engine
+app.set("view engine", "hbs");
+// menambahkan path
+app.set("views", "src/views");
+app.use("/assets", express.static("src/assets"));
+app.use(express.urlencoded({ extended: false })); // body parser
+app.use(
+  session({
     cookie: {
-        httpOnly: true,
-        secure: false,
-        maxAge: 2 * 60 * 60 * 1000,
+      httpOnly: true,
+      secure: false,
+      maxAge: 1 * 60 * 60 * 1000,
     },
     resave: false,
     store: session.MemoryStore(),
-    secret: 'session_storage',
+    secret: "session-storage",
     saveUninitialized: true,
-}));
+  })
+);
 app.use(flash());
 
-app.get('/', home);
-app.get('/contact', contact);
-app.get('/My-Project', MyProject);
-app.get('/My-Project-detail/:id', MyProjectDetail);
-app.get('/add-My-Project', addMyProject);
-app.post('/My-Project', handlePostMyProject);
-app.get('/delete-My-Project/:id', handleDeleteMyProject);
-app.get('/update-My-Project/:id', editMyProject);
-app.get('/register', formRegister);
-app.post('/register', addRegister);
-app.get('/login', formLogin);
-app.post('/login', isLogin);
+// routing
+app.get("/", home);
+app.get("/contact", contact);
+app.get("/my-project", MyProject);
+app.post("/my-project", handleMyProject);
 
-async function home(req, res) {
-    try {
-        const projectNew = await SequelizePool.query('SELECT * FROM myproject');
-        const titlePage = 'Home';
+app.get("/my-testimonials", myTestimonials);
+app.get("/detail-project/:id", detailProject);
+app.get("/register", register);
+app.post("/register", handleRegister);
+app.get("/login", login);
+app.post("/login", handleLogin);
+app.get("/logout", handleLogout);
 
-        res.render('index', {
-            data: projectNew[0],
-            titlePage,
-            isLogin: req.session.isLogin,
-            user: req.session.user
-        });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
-    }
+app.get("/delete/:id", handleDeleteProject);
+app.get("/edit-my-project/:id", editMyProject);
+app.post("/edit-my-project/:id", editMyProjectForm);
+
+function register(req, res) {
+  const titlePage = "Register";
+  res.render("register", {
+    titlePage,
+    handleLogin: req.session.handleLogin,
+    user: req.session.user,
+  });
 }
 
-function contact(req, res) {
-    const titlePage = 'Contact';
-    res.render('contact', {
-        titlePage,
-        isLogin: req.session.isLogin,
-        user: req.session.user
-    });
-}
-
-async function MyProject(req, res) {
-    try {
-        const query = await SequelizePool.query("SELECT * FROM myproject", { type: QueryTypes.SELECT });
-        const data = query.map(res => ({
-            ...res,
-            author: "Megawati",
-            image: "https://img.freepik.com/free-photo/modern-office-space-with-desktops-with-modern-computers-created-with-generative-ai-technology_185193-110089.jpg?w=826&t=st=1705553908~exp=1705554508~hmac=e65ecda5f1b0cc049b17c786b0674845bdd02f9ac3dcda91ed3ae13847e2c389"
-        }));
-
-        res.render('My-Project', {
-            data,
-            isLogin: req.session.isLogin,
-            user: req.session.user
-        });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
-    }
-}
-
-async function MyProjectDetail(req, res) {
-  const { id } = req.params;
-
+async function handleRegister(req, res) {
   try {
-      const query = await SequelizePool.query(`SELECT * FROM myproject WHERE id = ${id}`, { type: QueryTypes.SELECT });
+    const { name, email, password } = req.body;
 
-      if (query.length === -1) {
-          res.status(404).send('Project not found');
-          return;
-      }
+    console.log(password);
+    bcrypt.hash(password, 10, async function (err, hashPass) {
+      await SequelizePool.query(
+        `INSERT INTO public.user (name, email, password, "createdAt", "updatedAt")
+      VALUES ('${name}','${email}','${hashPass}' ,NOW(), NOW())`
+      );
+    });
 
-      const dataDetail = {
-          ...query[0],
-          author: "Megawati",
-          image: "https://img.freepik.com/free-photo/modern-office-space-with-desktops-with-modern-computers-created-with-generative-ai-technology_185193-110089.jpg?w=826&t=st=1705553908~exp=1705554508~hmac=e65ecda5f1b0cc049b17c786b0674845bdd02f9ac3dcda91ed3ae13847e2c389"
-      };
-
-      res.render('My-Project-detail', { data: dataDetail });
+    res.redirect("/login");
   } catch (error) {
-      console.error('Error:', error);
-      res.status(500).send('Internal Server Error');
+    throw error;
   }
 }
 
-
-function addMyProject(req, res) {
-    const addMyProjectTitle = "Add My Project";
-    res.render("add-My-Project", { data: addMyProjectTitle });
+function login(req, res) {
+  const titlePage = "Login";
+  res.render("login", {
+    titlePage,
+    handleLogin: req.session.handleLogin,
+    user: req.session.user,
+  });
 }
 
-async function handlePostMyProject(req, res) {
-    try {
-        const { title, content } = req.body;
-        await SequelizePool.query(`INSERT INTO myproject(project_name, start_date, description, "createdAt", "updatedAt") VALUES ('${title}','${content}', NOW(), NOW())`);
+async function handleLogin(req, res) {
+  try {
+    const { email, password } = req.body;
+    const checkEmail = await SequelizePool.query(
+      `SELECT * FROM public.user WHERE email = '${email}'`,
+      { type: QueryTypes.SELECT }
+    );
 
-        res.redirect('/My-Project');
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
+    if (!checkEmail.length) {
+      req.flash("failed", "Your Email Is Not Register!!!");
+      return res.redirect("/login");
     }
+
+    bcrypt.compare(password, checkEmail[0].password, function (err, result) {
+      if (!result) {
+        return res.redirect("/login");
+      } else {
+        req.session.handleLogin = true;
+        req.session.user = checkEmail[0].name;
+        req.flash("success", "👋🏻 Welcome!!! Now You Are Succesfully Logged To Your Account!");
+        return res.redirect("/");
+      }
+    });
+  } catch (error) {
+    throw error;
+  }
 }
 
-async function handleDeleteMyProject(req, res) {
-    try {
-        const { id } = req.params;
-        await SequelizePool.query(`DELETE FROM myproject WHERE id = ${id}`);
+async function home(req, res) {
+  const projectNew = await SequelizePool.query("SELECT * FROM public.myproject");
+  const titlePage = "Home";
 
-        res.redirect('/My-Project');
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
+  const dataNew = projectNew[0].map((res) => ({
+    ...res,
+    handleLogin: req.session.handleLogin,
+  }));
+
+  res.render("index", {
+    titlePage,
+    handleLogin: req.session.handleLogin,
+    user: req.session.user,
+    data: dataNew,
+  });
+}
+
+function contact(req, res) {
+  const titlePage = "Contact";
+  res.render("contact", {
+    titlePage,
+    handleLogin: req.session.handleLogin,
+    user: req.session.user,
+  });
+}
+
+async function MyProject(req, res) {
+  const titlePage = "My Project";
+
+  res.render("my-project", {
+    data: titlePage,
+    handleLogin: req.session.handleLogin,
+    user: req.session.user,
+  });
+}
+
+function myTestimonials(req, res) {
+  const titlePage = "My Testimonials";
+  res.render("my-testimonials", {
+    titlePage,
+    handleLogin: req.session.handleLogin,
+    user: req.session.user,
+  });
+}
+
+async function detailProject(req, res) {
+  const titlePage = "Detail Project";
+  const { id } = req.params;
+  const data = await SequelizePool.query(
+    "SELECT * FROM public.myproject where id = " + id
+  );
+
+  res.render("detail-project", {
+    data: data[0][0],
+    titlePage,
+    handleLogin: req.session.handleLogin,
+    user: req.session.user,
+  });
+}
+
+async function handleMyProject(req, res) {
+  try {
+    const { projectName, startDate, endDate, description, techIcon } = req.body;
+
+    const dateOne = new Date(startDate);
+    const dateTwo = new Date(endDate);
+    const time = Math.abs(dateTwo - dateOne);
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const months = Math.floor(time / (1000 * 60 * 60 * 24 * 30));
+    const years = Math.floor(time / (1000 * 60 * 60 * 24) / 365);
+
+    let duration = [];
+
+    if (days < 24) {
+      duration += days + " Days";
+    } else if (months < 12) {
+      duration += months + " Month";
+    } else if (years < 365) {
+      duration += years + " Years";
     }
+
+    await SequelizePool.query(
+      `INSERT INTO public.myproject(project_name, start_date,end_date,description,technologies, "createdAt", "updatedAt",duration) 
+      VALUES ('${projectName}','${startDate}','${endDate}' ,'${description}','{${techIcon}}',NOW(), NOW(), '${duration}')`
+    );
+    res.redirect("/");
+  } catch (error) {
+    throw error;
+  }
 }
 
-function editMyProject(req, res) {
-    res.render("update-My-Project");
+async function editMyProject(req, res) {
+  const { id } = req.params;
+  const data = await SequelizePool.query(
+    "SELECT * FROM public.myproject where id = " + id
+  );
+
+  res.render("edit-my-project", {
+    data: data[0][0],
+    handleLogin: req.session.handleLogin,
+    user: req.session.user,
+  });
 }
 
-function formRegister(req, res) {
-    res.render('register');
-}
+async function editMyProjectForm(req, res) {
+  try {
+    const { id } = req.params;
 
-async function addRegister(req, res) {
-    try {
-        const { name, email, password } = req.body;
-        const salt = 10;
+    const { projectName, startDate, endDate, description, techIcon } = req.body;
 
-        bcrypt.hash(password, salt, async (err, hashPassword) => {
-            await SequelizePool.query(`INSERT INTO user (name, email, password, "createdAt", "updatedAt") VALUES ('${name}','${email}','${hashPassword}', NOW(), NOW())`);
-        });
-        res.redirect('/login');
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
+    const dateOne = new Date(startDate);
+    const dateTwo = new Date(endDate);
+    const time = Math.abs(dateTwo - dateOne);
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const months = Math.floor(time / (1000 * 60 * 60 * 24 * 30));
+    const years = Math.floor(time / (1000 * 60 * 60 * 24) / 365);
+
+    let duration = [];
+
+    if (days < 24) {
+      duration += days + " Days";
+    } else if (months < 12) {
+      duration += months + " Month";
+    } else if (years < 365) {
+      duration += years + " Years";
     }
+
+    await SequelizePool.query(
+      `UPDATE public.myproject SET project_name='${projectName}', start_date='${startDate}', end_date='${endDate}', 
+      description='${description}',"updatedAt"=now(), duration='${duration}', technologies='{${techIcon}}' where id = ${id}`
+    );
+    res.redirect("/");
+  } catch (error) {
+    throw error;
+  }
 }
 
-function formLogin(req, res) {
-    res.render('login');
+async function handleDeleteProject(req, res) {
+  const { id } = req.params;
+  const data = await SequelizePool.query(
+    "DELETE FROM public.myproject where id = " + id
+  );
+
+  res.redirect("/");
 }
 
-async function isLogin(req, res) {
-    try {
-        const { email, password } = req.body;
-
-        const checkEmail = await SequelizePool.query(`SELECT * FROM user WHERE email = '${email}'`, { type: QueryTypes.SELECT });
-
-        if (!checkEmail.length) {
-            req.flash('failed', 'Email is not registered');
-            return res.redirect('/login');
-        }
-
-        bcrypt.compare(password, checkEmail[0].password, function (err, result) {
-            if (!result) {
-                req.flash('failed', 'Invalid password');
-                return res.redirect("/login");
-            } else {
-                req.session.isLogin = true;
-                req.session.user = checkEmail[0].name;
-                req.flash('success', 'Welcome bwangggggg !!!!');
-                return res.redirect('/');
-            }
-        });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
-    }
+function handleLogout(req, res) {
+  req.session.handleLogin = false;
+  req.session.user = null;
+  res.redirect("/");
 }
 
 app.listen(port, () => {
-    console.log(`Server berjalan di port ${port}`);
+  console.log(`Server Berjalan Di Port ${port}`);
 });
